c1d49387d6bfbbcac828257cac8b48606ade0724
hxc <249270087@qq.com>
2019-01-06 21:36:02 +0800

    修改GzipUtil

--- src/main/java/com/limaila/support/global/compress/utils/GzipUtil.java
+++ src/main/java/com/limaila/support/global/compress/utils/GzipUtil.java
@@ -14,12 +14,30 @@ import java.util.zip.GZIPOutputStream;
 @Slf4j
 public class GzipUtil {
 
-    public static final String compress(String src)  {
+    /**
+     * 压缩数据且通过Base64加密
+     * @param src 源数据
+     * @return
+     */
+    public static final String compressAndBase64(String src)  {
         byte[] bytes = compress(src.getBytes());
         return Base64.getEncoder().encodeToString(bytes);
     }
 
+    /**
+     * 压缩数据
+     * @param src 源数据
+     * @return
+     */
+    public static final byte[] compress(String src) {
+        return compress(src.getBytes());
+    }
 
+    /**
+     * 压缩数据
+     * @param bytes 源数据
+     * @return
+     */
     public static final byte[] compress(byte[] bytes) {
         ByteArrayOutputStream aos = new ByteArrayOutputStream();
         GZIPOutputStream gos;
@@ -42,13 +60,31 @@ public class GzipUtil {
         return bs;
     }
 
-    public static final String uncompress(String src) {
+    /**
+     * 解压数据并且通过Base64解码
+     * @param src 源数据
+     * @return
+     */
+    public static final String uncompressAndBase64(String src) {
         byte[] decode = Base64.getDecoder().decode(src);
         byte[] bytes = uncompress(decode);
         return new String(bytes);
     }
 
+    /**
+     * 解压缩数据
+     * @param src 源数据
+     * @return
+     */
+    public static final byte[] uncompress(String src) {
+        return uncompress(src.getBytes());
+    }
 
+    /**
+     * 解压缩数据
+     * @param bs 源数据
+     * @return
+     */
     public static final byte[] uncompress(byte[] bs) {
         if (bs == null || bs.length == 0) {
             return null;
@@ -70,10 +106,10 @@ public class GzipUtil {
 
     public static void main(String[] args) throws UnsupportedEncodingException {
         String str = "我是人哈哈哈哈哈哈呵呵%5B%7B%22lastUpdateTime%22%3A%222011-10-28+9%3A39%3A41%22%2C%22smsList%22%3A%5B%7B%22liveState%22%3A%221AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-        System.out.println("压缩前 ：" + str);
-        String compress = compress(str);
-        System.out.println("压缩后 ：" + compress);
-        String uncompress = uncompress(compress);
-        System.out.println("解压后 : " + uncompress);
+        System.out.println("压缩前 ：" + str.getBytes().length);
+        byte[] compress = compress(str);
+        System.out.println("压缩后 ：" + compress.length);
+        byte[] uncompress = uncompress(compress);
+        System.out.println("解压后 : " + new String(uncompress).getBytes().length);
     }
 }
