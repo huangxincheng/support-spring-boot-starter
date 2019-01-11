2a6c7dcb73ac32d88a61441d02dc509b9a4a1f78
黄信程 <huangxincheng@xmles.cn>
2019-01-11 10:35:48 +0800

    修改

--- src/main/java/com/limaila/support/global/log/annotation/StatLog.java
+++ src/main/java/com/limaila/support/global/log/annotation/StatLog.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
  * <p>
  * <p>
  **/
-@Target(value = {ElementType.METHOD})
+@Target(value = {ElementType.METHOD,ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface StatLog {
 
